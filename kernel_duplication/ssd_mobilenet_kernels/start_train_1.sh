737324953e496ba12f0783b01542f6ab9df73cde
Wei Chen <cweinone@gmail.com>
2020-07-26 14:10:04 -0400

    Train layer 1-11

--- kernel_duplication/ssd_mobilenet_kernels/start_train_1.sh
+++ kernel_duplication/ssd_mobilenet_kernels/start_train_1.sh
@@ -1,19 +1,22 @@
 export CUDA_VISIBLE_DEVICES=1
 IDX=12
 
-python train_ssd.py --dataset_type voc \
-        --datasets /home/rtml/data/VOCdevkit/VOC2007 \
-        --validation_dataset /home/rtml/data/VOCdevkit/VOC2007 \
-        --net mb1-ssd \
-        --pretrained_ssd models/mobilenet-v1-ssd-mp-0_675.pth \
-        --checkpoint_folder models \
-        --scheduler cosine \
-        --lr 0.01 \
-        --batch_size 32 \
-        --t_max 200 \
-        --validation_epochs 50 \
-        --num_epochs 50 \
-        --run_original False \
-        --use_cuda True \
-        --weight_index ${IDX}
+for IDX in {1..11}
+do
+    python train_ssd.py --dataset_type voc \
+            --datasets /home/rtml/data/VOCdevkit/VOC2007 \
+            --validation_dataset /home/rtml/data/VOCdevkit/VOC2007 \
+            --net mb1-ssd \
+            --pretrained_ssd models/mobilenet-v1-ssd-mp-0_675.pth \
+            --checkpoint_folder models \
+            --scheduler cosine \
+            --lr 0.01 \
+            --batch_size 32 \
+            --t_max 200 \
+            --validation_epochs 50 \
+            --num_epochs 10 \
+            --run_original False \
+            --use_cuda True \
+            --weight_index ${IDX}
+done
 
