f7215fdbaebc0432458b0775895f2314677fcd02
Wei Chen <cweinone@gmail.com>
2020-07-26 22:33:25 -0400

    DEBUG

--- kernel_duplication/ssd_vgg/compare_dup_attention_kernel.sh
+++ kernel_duplication/ssd_vgg/compare_dup_attention_kernel.sh
@@ -23,7 +23,7 @@ export CUDA_VISIBLE_DEVICES=1
 #python eval_attention_kernel.py --attention_mode True --error_rate 0.98 --touch_layer_index 1 --ft_type attention
 #python eval_attention_kernel.py --attention_mode True --error_rate 1 --touch_layer_index 1 --ft_type attention
 ##
-python eval_attention_kernel.py --attention_mode True --error_rate 0.1 --touch_layer_index 1 --ft_type importance
+#python eval_attention_kernel.py --attention_mode True --error_rate 0.1 --touch_layer_index 1 --ft_type importance
 #python eval_subflow_d2nn.py --attention_mode True --error_rate 0.3 --touch_layer_index 1 --ft_type importance
 #python eval_subflow_d2nn.py --attention_mode True --error_rate 0.5 --touch_layer_index 1 --ft_type importance
 #python eval_subflow_d2nn.py --attention_mode True --error_rate 0.7 --touch_layer_index 1 --ft_type importance
@@ -34,7 +34,7 @@ python eval_attention_kernel.py --attention_mode True --error_rate 0.1 --touch_l
 #python eval_subflow_d2nn.py --attention_mode True --error_rate 0.98 --touch_layer_index 1 --ft_type importance
 #python eval_subflow_d2nn.py --attention_mode True --error_rate 1 --touch_layer_index 1 --ft_type importance
 ##
-#python eval_subflow_d2nn.py --attention_mode True --error_rate 0.1 --touch_layer_index 1 --ft_type d2nn
+python eval_attention_kernel.py --attention_mode True --error_rate 0.1 --touch_layer_index 1 --ft_type d2nn
 #python eval_subflow_d2nn.py --attention_mode True --error_rate 0.3 --touch_layer_index 1 --ft_type d2nn
 #python eval_subflow_d2nn.py --attention_mode True --error_rate 0.5 --touch_layer_index 1 --ft_type d2nn
 #python eval_subflow_d2nn.py --attention_mode True --error_rate 0.7 --touch_layer_index 1 --ft_type d2nn
