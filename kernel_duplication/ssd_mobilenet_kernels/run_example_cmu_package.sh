fa819b490428270e91e23569da39ddeff1ce202f
Wei Chen <cweinone@gmail.com>
2020-07-30 11:55:55 -0400

    DEBUG

--- kernel_duplication/ssd_mobilenet_kernels/run_example_cmu_package.sh
+++ kernel_duplication/ssd_mobilenet_kernels/run_example_cmu_package.sh
@@ -6,5 +6,5 @@ CUDA_VISIBLE_DEVICES=1
 python run_ssd_example_package.py mb1-ssd \
                                   ${MODEL} \
                                   models/voc-model-labels.txt \
-                                  /home/rtml/data/VOCdevkit/VOC2007 \
+                                  /home/rtml/data/VOCdevkit/VOC2007/ \
                                   detection/
