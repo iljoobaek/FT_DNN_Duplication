117ee8312eeda25b73dc6299965f893733df6778
Wei Chen <cweinone@gmail.com>
2020-08-01 18:12:26 -0400

    Vgg error feature map

--- kernel_duplication/ssd_vgg/comp_error_weight_feature.sh
+++ kernel_duplication/ssd_vgg/comp_error_weight_feature.sh
@@ -3,5 +3,5 @@ IDX=5
 
 for ERR in $(seq 0 0.01 1)
 do
-    python eval_attention_kernel.py --attention_mode False --error_rate 0 --touch_layer_index 1 --weight_index ${IDX} --weight_error ${ERR}
+    python eval_attention_kernel.py --attention_mode False --error_rate ${ERR} --touch_layer_index 1 --weight_index ${IDX} --weight_error 0
 done
