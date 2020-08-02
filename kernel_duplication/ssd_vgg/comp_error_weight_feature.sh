36f752dcde758d4acab493484119183ab8647282
Wei Chen <cweinone@gmail.com>
2020-08-02 11:21:59 -0400

    Vgg feature map

--- kernel_duplication/ssd_vgg/comp_error_weight_feature.sh
+++ kernel_duplication/ssd_vgg/comp_error_weight_feature.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES=1
-IDX=5
+export CUDA_VISIBLE_DEVICES=0
+IDX=1
 
-for ERR in $(seq 0 0.01 1)
+for ERR in $(seq 0 0.1 1)
 do
     python eval_attention_kernel.py --attention_mode False --error_rate ${ERR} --touch_layer_index 1 --weight_index ${IDX} --weight_error 0
 done
