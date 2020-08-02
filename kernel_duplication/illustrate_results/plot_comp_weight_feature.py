98247a2ebeba4f4dbbeb15d7bb705170533c7ec8
Wei Chen <cweinone@gmail.com>
2020-08-02 18:19:26 -0400

    MB 0 error weight

--- kernel_duplication/illustrate_results/plot_comp_weight_feature.py
+++ kernel_duplication/illustrate_results/plot_comp_weight_feature.py
@@ -11,58 +11,58 @@ path = "results_weight_feature.txt"
 width = 0
 error = 0.9
 
-# y_att_all = []
-# y_imp_all = []
+y_fea_all = []
+y_wei_all = []
 # y_sco_all = []
 labels = []
-y_ori = []
-y_att = []
-y_imp = []
-y_sco = []
-y_rad = []
+y_fea = []
+y_wei = []
+# y_ori = []
+# y_att = []
+# y_imp = []
+# y_sco = []
+# y_rad = []
 with open(path, 'r') as f:
     for line in f:
-        if "Layer" in line:
+        if "type" in line:
+            y_fea_all.append(y_fea)
+            y_wei_all.append(y_wei)
+            y_wei = []
+            y_fea = []
             width += 1
             continue
         arr = line.strip().split("|")
-        if arr[0] == str(error):
-            if arr[1] == "False":
-                y_ori.append(float(arr[3]))
-            else:
-                if arr[2] == "attention":
-                    y_att.append(float(arr[3]))
-                elif arr[2] == "importance":
-                    y_imp.append(float(arr[3]))
-                elif arr[2] == "random":
-                    y_rad.append(float(arr[3]))
-                else:
-                    y_sco.append(float(arr[3]))
+        if arr[1] == "False":
+            y_fea.append(float(arr[3]))
+        else:
+            y_wei.append(float(arr[3]))
 
-n_plots = 1
-plt.figure(figsize=(4.8, 4.2))
-x = np.arange(1, width + 1)
+n_plots = 2
+plt.figure(figsize=(4.8 * n_plots, 4.2))
+# x = np.arange(1, width + 1)
+x = np.arange(0, 1.1, 0.1)
+error_type = ["zero error", "random error"]
 
 for i in range(n_plots):
     plt.subplot(1, n_plots, i + 1)
-    data = {'x': np.concatenate((np.array(x), np.array(x), np.array(x), np.array(x), np.array(x))),
-            'y': np.concatenate((np.array(y_ori), np.array(y_att), np.array(y_imp), np.array(y_sco), np.array(y_rad))),
-            'method': ["original"] * len(x) + ["attention"] * len(x) + ["importance"] * len(x) + ["d2nn"] * len(x) + ["random"] * len(x)}
+    data = {'x': np.concatenate((np.array(x), np.array(x), np.array(x))),
+            'y': np.concatenate((np.array(y_fea_all[i]), np.array(y_wei_all[i]), np.array([y_wei_all[i][0]] * len(x)))),
+            'type': ["error at feature map"] * len(x) + ["error at weight"] * len(x) + ["no error"] * len(x)}
     # data = {'x': np.concatenate((np.array(x), np.array(x), np.array(x), np.array(x))),
     #         'y': np.concatenate((np.array(y_ori), np.array(y_imp), np.array(y_sco), np.array(y_rad))),
     #         'method': ["original"] * len(x) + ["importance"] * len(x) + ["d2nn"] * len(x) + ["random"] * len(x)}
 
     df = pd.DataFrame(data)
 
-    ax = sns.lineplot(x='x', y='y', hue='method', data=df)
+    ax = sns.lineplot(x='x', y='y', hue='type', data=df)
     ax.spines['top'].set_color('black')
     ax.spines['bottom'].set_color('black')
     ax.spines['left'].set_color('black')
     ax.spines['right'].set_color('black')
 
-    plt.xlabel('Layer Index', fontsize=18, fontweight='bold')
+    plt.xlabel('Error Rate (%)', fontsize=18, fontweight='bold')
     plt.ylabel('Accuracy (%)', fontsize=18, fontweight='bold')
-    plt.title('Feature Map Error=' + str(error), fontsize=24, fontweight='bold')
+    plt.title('Error Type: ' + error_type[i], fontsize=24, fontweight='bold')
     plt.tick_params(labelsize=18)
     plt.ylim(0, 0.8)
 
