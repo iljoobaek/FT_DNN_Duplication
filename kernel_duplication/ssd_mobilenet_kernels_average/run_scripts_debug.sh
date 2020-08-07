14b8c2b6827495d1dd9fe5db0a8573eea6a37ae1
Wei Chen <cweinone@gmail.com>
2020-08-07 10:53:30 -0400

    DEBUG

--- kernel_duplication/ssd_mobilenet_kernels_average/run_scripts_debug.sh
+++ kernel_duplication/ssd_mobilenet_kernels_average/run_scripts_debug.sh
@@ -1,5 +1,5 @@
 NUM_DUP=0.5
-IDX=11
+IDX=1
 MODEL=models/attention3/ssd300_COCO_9.pth
 ATMODEL=models/attention3/${IDX}_ssd300_COCO_9.pth
 python eval_ssd.py --error_rate 0.1 --percent_duplication 0 --run_original False --duplicated False --ft_type none --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error 0.01
