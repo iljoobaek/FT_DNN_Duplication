735927d0612ecbd9eac853dfdca61678f52ded28
Wei Chen <cweinone@gmail.com>
2020-08-13 11:37:59 -0400

    DEBUG

--- kernel_duplication/ssd_mobilenet_kernels_average/run_scripts_kernel_avg.sh
+++ kernel_duplication/ssd_mobilenet_kernels_average/run_scripts_kernel_avg.sh
@@ -4,18 +4,19 @@ IDX=1
 MODEL=models/attention3/ssd300_COCO_9.pth
 ATMODEL=models/attention3/${IDX}_ssd300_COCO_9.pth
 
-for WERR in $(seq 0.01 0.01 0.03)
-do
+WERR=0.01
+#for WERR in $(seq 0.01 0.01 0.03)
+#do
     echo "error=${WERR}" >> result.txt
-    for ERR in $(seq 0.01 0.01 0.1)
+    for ERR in $(seq 0.06 0.01 0.1)
     do
         python eval_ssd.py --error_rate ${ERR} --percent_duplication 0 --run_original False --duplicated False --ft_type none --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error ${WERR}
 
-        python eval_ssd.py --error_rate ${ERR} --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type importance --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error ${WERR}
+#        python eval_ssd.py --error_rate ${ERR} --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type importance --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error ${WERR}
 
         python eval_ssd.py --error_rate ${ERR} --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type d2nn --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error ${WERR}
 
         python eval_ssd.py --error_rate ${ERR} --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type random --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error ${WERR}
     done
 
-done
+#done
