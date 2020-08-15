5169dcb915bbf8195191cff44ae1a9a58f25824b
Wei Chen <cweinone@gmail.com>
2020-08-15 11:12:28 -0400

    Layerwise entropy

---
+++ kernel_duplication/ssd_mobilenet_entropy_method/plot.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns; sns.set()
+
+sns.set_style('whitegrid')
+
+path = "results_1.txt"
+# x = [0.1, 0.3, 0.5, 0.7, 0.9, 0.92, 0.94, 0.96, 0.98, 1]
+x = [0.1, 0.3, 0.5, 0.7, 0.9, 1]
+
+y_att_all = []
+y_imp_all = []
+y_sco_all = []
+labels = []
+y_ori = []
+y_att = []
+y_imp = []
+y_sco = []
+with open(path, 'r') as f:
+    for line in f:
+        if "#duplication" in line:
+            y_att_all.append(y_att)
+            y_imp_all.append(y_imp)
+            y_sco_all.append(y_sco)
+            y_att = []
+            y_imp = []
+            y_sco = []
+            labels.append(line.strip()[13:])
+            continue
+        arr = line.strip().split("|")
+        if arr[1] == "False":
+            y_ori.append(float(arr[3]))
+        else:
+            if arr[2] == "random":
+                y_att.append(float(arr[3]))
+            elif arr[2] == "importance":
+                y_imp.append(float(arr[3]))
+            else:
+                y_sco.append(float(arr[3]))
+
+n_plots = len(labels)
+plt.figure(figsize=(4.8*n_plots, 4.2))
+
+for i in range(n_plots):
+    plt.subplot(1, n_plots, i + 1)
+    data = {'x': np.concatenate((np.array(x), np.array(x), np.array(x), np.array(x))),
+            'y': np.concatenate((np.array(y_ori), np.array(y_att_all[i]), np.array(y_imp_all[i]), np.array(y_sco_all[i]))),
+            'method': ["original"] * len(x) + ["random"] * len(x) + ["importance"] * len(x) + ["d2nn"] * len(x)}
+    df = pd.DataFrame(data)
+
+    ax = sns.lineplot(x='x', y='y', hue='method', data=df)
+    ax.spines['top'].set_color('black')
+    ax.spines['bottom'].set_color('black')
+    ax.spines['left'].set_color('black')
+    ax.spines['right'].set_color('black')
+
+    plt.xlabel('Error rate (%)', fontsize=18, fontweight='bold')
+    plt.ylabel('Accuracy (%)', fontsize=18, fontweight='bold')
+    plt.title(str(labels[i]) + ' duplications', fontsize=24, fontweight='bold')
+    plt.tick_params(labelsize=18)
+    plt.ylim(0, 0.8)
+
+plt.tight_layout()
+#plt.show()
+plt.savefig('result_touch_1.pdf')
---
+++ kernel_duplication/ssd_mobilenet_entropy_method/run_scripts_all_layers.sh
@@ -0,0 +1,50 @@
+NUM_DUP=0.5
+IDX=1
+MODEL=models/attention3/ssd300_COCO_9.pth
+
+ATMODEL=models/attention3/${IDX}_ssd300_COCO_9.pth
+for WERR in $(seq 0.02 0.01 0.1)
+do
+echo "error=${WERR}" >> result.txt
+for ERR in $(seq 0.01 0.01 0.1)
+do
+#python eval_ssd.py --error_rate 0 --percent_duplication 0 --run_original False --duplicated False --ft_type none --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+python eval_ssd.py --error_rate ${ERR} --percent_duplication 0 --run_original False --duplicated False --ft_type none --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error ${WERR}
+#python eval_ssd.py --error_rate 0.3 --percent_duplication 0 --run_original False --duplicated False --ft_type none --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.5 --percent_duplication 0 --run_original False --duplicated False --ft_type none --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.7 --percent_duplication 0 --run_original False --duplicated False --ft_type none --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.9 --percent_duplication 0 --run_original False --duplicated False --ft_type none --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 1 --percent_duplication 0 --run_original False --duplicated False --ft_type none --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+
+#python eval_ssd.py --error_rate 0.1 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type attention --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.3 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type attention --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.5 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type attention --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.7 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type attention --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.9 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type attention --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 1 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type attention --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+
+python eval_ssd.py --error_rate ${ERR} --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type importance --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error ${WERR}
+#python eval_ssd.py --error_rate 0.3 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type importance --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.5 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type importance --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.7 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type importance --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.9 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type importance --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 1 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type importance --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+
+
+#echo #duplication=${NUM_DUP} >> result.txt
+python eval_ssd.py --error_rate ${ERR} --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type d2nn --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error ${WERR}
+#python eval_ssd.py --error_rate 0.3 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type d2nn --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.5 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type d2nn --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.7 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type d2nn --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.9 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type d2nn --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 1 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type d2nn --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+
+
+python eval_ssd.py --error_rate ${ERR} --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type random --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX} --weight_error ${WERR}
+#python eval_ssd.py --error_rate 0.3 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type random --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.5 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type random --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.7 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type random --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 0.9 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type random --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+#python eval_ssd.py --error_rate 1 --percent_duplication ${NUM_DUP} --run_original False --duplicated True --ft_type random --dataset_type voc --dataset /home/rtml/data/VOCdevkit/VOC2007 --net mb1-ssd --trained_model ${ATMODEL} --label_file models/voc-model-labels.txt --weight_index ${IDX}
+done
+done
