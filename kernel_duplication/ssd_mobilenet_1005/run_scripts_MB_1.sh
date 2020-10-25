22d5bd8a8d9eb52e6e006860bd3176287b821712
Manoj <manojbhat09@gmail.com>
2020-10-25 14:32:07 -0400

    new exp

--- kernel_duplication/ssd_mobilenet_1005/run_scripts_MB_1.sh
+++ kernel_duplication/ssd_mobilenet_1005/run_scripts_MB_1.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
-NUM_DUP=0.5
+NUM_DUP=0.05
 IDX=1
 # MODEL=models/attention3/ssd300_COCO_9.pth
 MODEL=models/vgg16-ssd-mp-0_7726.pth
@@ -14,7 +14,7 @@ echo "Kernel Only" >> ${SAVEFILE}
 #do
 for SEED in $(seq 1 1 1)
 do
-for WERR in $(seq 0.01 0.01 0.1)
+for WERR in $(seq 0.01 0.01 0.01)
 # WERR=0.02
 # for NUM_DUP in $(seq 0.1 0.2 0.1)
 do
@@ -58,27 +58,27 @@ do
 
 # done
 
-# echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=D2NN" >> ${SAVEFILE}
+echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=D2NN" >> ${SAVEFILE}
 
-# for ERR in $(seq 0.05 0.01 0.05)
-# do
-# python eval_ssd.py --error_rate ${ERR} \
-#                    --percent_duplication ${NUM_DUP} \
-#                    --run_original False \
-#                    --duplicated True \
-#                    --ft_type d2nn \
-#                    --dataset_type voc \
-#                    --dataset ${DATAPATH} \
-#                    --net mb1-ssd \
-#                    --trained_model ${ATMODEL} \
-#                    --label_file models/voc-model-labels.txt \
-#                    --weight_index ${IDX} \
-#                    --weight_error ${WERR} \
-#                    --result_save_file ${SAVEFILE} \
-#                    --seed ${SEED} \
-#                    --recover_type KR
+for ERR in $(seq 0.01 0.01 0.05)
+do
+python eval_ssd.py --error_rate ${ERR} \
+                   --percent_duplication ${NUM_DUP} \
+                   --run_original False \
+                   --duplicated True \
+                   --ft_type d2nn \
+                   --dataset_type voc \
+                   --dataset ${DATAPATH} \
+                   --net mb1-ssd \
+                   --trained_model ${ATMODEL} \
+                   --label_file models/voc-model-labels.txt \
+                   --weight_index ${IDX} \
+                   --weight_error ${WERR} \
+                   --result_save_file ${SAVEFILE} \
+                   --seed ${SEED} \
+                   --recover_type KR
 
-# done
+done
 
 # for ERR in $(seq 0.1 0.01 0.1)
 # do
@@ -100,25 +100,25 @@ do
 
 # done
 
-echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=Importance" >> ${SAVEFILE}
-for ERR in $(seq 0.01 0.01 0.01)
-do
-python eval_ssd.py --error_rate ${ERR} \
-                   --percent_duplication ${NUM_DUP} \
-                   --run_original False \
-                   --duplicated True \
-                   --ft_type importance \
-                   --dataset_type voc \
-                   --dataset ${DATAPATH} \
-                   --net mb1-ssd \
-                   --trained_model ${ATMODEL} \
-                   --label_file models/voc-model-labels.txt \
-                   --weight_index ${IDX} \
-                   --weight_error ${WERR} \
-                   --result_save_file ${SAVEFILE} \
-                   --seed ${SEED} \
-                   --recover_type FMR
-done
+# echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=Importance" >> ${SAVEFILE}
+# for ERR in $(seq 0.01 0.01 0.01)
+# do
+# python eval_ssd.py --error_rate ${ERR} \
+#                    --percent_duplication ${NUM_DUP} \
+#                    --run_original False \
+#                    --duplicated True \
+#                    --ft_type importance \
+#                    --dataset_type voc \
+#                    --dataset ${DATAPATH} \
+#                    --net mb1-ssd \
+#                    --trained_model ${ATMODEL} \
+#                    --label_file models/voc-model-labels.txt \
+#                    --weight_index ${IDX} \
+#                    --weight_error ${WERR} \
+#                    --result_save_file ${SAVEFILE} \
+#                    --seed ${SEED} \
+#                    --recover_type FMR
+# done
 
 # echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=Entropy" >> ${SAVEFILE}
 # for ERR in $(seq 0.01 0.01 0.1)
