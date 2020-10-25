6445246f15ff158d801dee3ff0d9f4e807e48df6
Manoj <manojbhat09@gmail.com>
2020-10-25 00:52:39 -0400

    new exp

--- kernel_duplication/ssd_mobilenet_1005/run_scripts_MB.sh
+++ kernel_duplication/ssd_mobilenet_1005/run_scripts_MB.sh
@@ -38,26 +38,26 @@ do
 #                    --seed ${SEED}
 # done
 
-# echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=Random" >> ${SAVEFILE}
-# for ERR in $(seq 0.01 0.01 0.1)
-# do
-# python eval_ssd.py --error_rate ${ERR} \
-#                    --percent_duplication ${NUM_DUP} \
-#                    --run_original False \
-#                    --duplicated True \
-#                    --ft_type random \
-#                    --dataset_type voc \
-#                    --dataset ${DATAPATH} \
-#                    --net mb1-ssd \
-#                    --trained_model ${ATMODEL} \
-#                    --label_file models/voc-model-labels.txt \
-#                    --weight_index ${IDX} \
-#                    --weight_error ${WERR} \
-#                    --result_save_file ${SAVEFILE} \
-#                    --seed ${SEED} \
-#                    --recover_type FMR
+echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=Random" >> ${SAVEFILE}
+for ERR in $(seq 0.01 0.01 0.1)
+do
+python eval_ssd.py --error_rate ${ERR} \
+                   --percent_duplication ${NUM_DUP} \
+                   --run_original False \
+                   --duplicated True \
+                   --ft_type random \
+                   --dataset_type voc \
+                   --dataset ${DATAPATH} \
+                   --net mb1-ssd \
+                   --trained_model ${ATMODEL} \
+                   --label_file models/voc-model-labels.txt \
+                   --weight_index ${IDX} \
+                   --weight_error ${WERR} \
+                   --result_save_file ${SAVEFILE} \
+                   --seed ${SEED} \
+                   --recover_type FMR
 
-# done
+done
 
 # echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=D2NN" >> ${SAVEFILE}
 
@@ -120,25 +120,25 @@ do
 #                    --seed ${SEED}
 # done
 
-echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=Entropy" >> ${SAVEFILE}
-for ERR in $(seq 0.01 0.01 0.1)
-do
-python eval_ssd.py --error_rate ${ERR} \
-                   --percent_duplication ${NUM_DUP} \
-                   --run_original False \
-                   --duplicated True \
-                   --ft_type entropy_p \
-                   --dataset_type voc \
-                   --dataset ${DATAPATH} \
-                   --net mb1-ssd \
-                   --trained_model ${ATMODEL} \
-                   --label_file models/voc-model-labels.txt \
-                   --weight_index ${IDX} \
-                   --weight_error ${WERR} \
-                   --result_save_file ${SAVEFILE} \
-                   --seed ${SEED} \
-                   --recover_type FMR
-done
+# echo "Error=${WERR}, Dup percentage=${NUM_DUP}, Type=Entropy" >> ${SAVEFILE}
+# for ERR in $(seq 0.01 0.01 0.1)
+# do
+# python eval_ssd.py --error_rate ${ERR} \
+#                    --percent_duplication ${NUM_DUP} \
+#                    --run_original False \
+#                    --duplicated True \
+#                    --ft_type entropy_p \
+#                    --dataset_type voc \
+#                    --dataset ${DATAPATH} \
+#                    --net mb1-ssd \
+#                    --trained_model ${ATMODEL} \
+#                    --label_file models/voc-model-labels.txt \
+#                    --weight_index ${IDX} \
+#                    --weight_error ${WERR} \
+#                    --result_save_file ${SAVEFILE} \
+#                    --seed ${SEED} \
+#                    --recover_type FMR
+# done
 
 done
 done
